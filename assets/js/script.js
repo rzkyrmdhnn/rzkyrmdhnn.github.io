0f5f49c5fc132e0e985b54c63b91454abcdbe659
DELL <rr037237@gmail.com>
2024-08-23 00:45:30 +0700

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -118,23 +118,22 @@ for (let i = 0; i < navigationLinks.length; i++) {
 }
 
 // Fungsi untuk menampilkan pesan selamat datang
-function welcomeMessage() {
-  alert("Welcome to my portfolio");
-  askName();
-}
+// function welcomeMessage() {
+//   alert("Welcome to my portfolio");
+//   askName();
+// }
 
-// Fungsi untuk menampilkan alert dengan input nama
-function askName() {
-  var name = prompt("What's your name?", "");
-  while (name == null || name == "") {
-      alert("Name was not entered. Please enter your name.");
-      name = prompt("What's your name?", "");
-  }
-  alert("Hello, " + name + "! 😊");
-}
+// // Fungsi untuk menampilkan alert dengan input nama
+// function askName() {
+//   var name = prompt("What's your name?", "");
+//   while (name == null || name == "") {
+//       alert("Name was not entered. Please enter your name.");
+//       name = prompt("What's your name?", "");
+//   }
+//   alert("Hello, " + name + "! 😊");
+// }
 
-// Panggil fungsi welcomeMessage saat halaman dimuat
-window.onload = welcomeMessage;
+// window.onload = welcomeMessage;
 
 // Fungsi untuk menampilkan pesan selamat datang
 // function welcomeMessage() {
